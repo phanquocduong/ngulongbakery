72f1dbc7038b289108fbc2cd7d20164e2aa44e90
Phan Quoc Duong <phanquocduong.070905@gmail.com>
2024-11-07 21:16:43 +0700

    collection page

---
+++ assets/js/collection.js
@@ -0,0 +1,9 @@
+function filterCategory() {
+    const filterCategoryBox = document.getElementById('filterCategoryBox');
+    filterCategoryBox.classList.toggle('filter-menu__body--active');
+}
+
+function filterPrice() {
+    const filterPriceBox = document.getElementById('filterPriceBox');
+    filterPriceBox.classList.toggle('filter-menu__body--active');
+}
