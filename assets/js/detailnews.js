1eda855eff6b2eb4026cc87c82f57a47e305b147
hailam2706 <hlamm2706@gmail.com>
2024-11-11 10:33:05 +0700

    Chi tiết News, News

--- assets/js/detailnews.js
+++ assets/js/detailnews.js
@@ -1,27 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-const indexListContainer = document.querySelector(".contentnews-listindex");
-const headings = document.querySelectorAll(".contentnews-titlecontent");
+    const indexListContainer = document.querySelector(".contentnews-listindex");
+    const headings = document.querySelectorAll(".contentnews-titlecontent");
+    const toggleButton = document.getElementById("toggle-button");
 
-const indexList = document.createElement("div");
-indexList.classList.add("index-list");
+    const indexList = document.createElement("div");
+    indexList.classList.add("index-list");
 
-headings.forEach((heading, index) => {
-    if (!heading.id) {
-    heading.id = `heading-${index + 1}`;
-    }
+    headings.forEach((heading, index) => {
+        if (!heading.id) {
+            heading.id = `heading-${index + 1}`;
+        }
 
-    const listItem = document.createElement("div");
-    const link = document.createElement("a");
-    link.href = `#${heading.id}`;
-    link.innerText = heading.innerText;
-    listItem.appendChild(link);
-    indexList.appendChild(listItem);
+        const listItem = document.createElement("div");
+        const link = document.createElement("a");
+        link.href = `#${heading.id}`;
+        link.innerText = heading.innerText;
+        listItem.appendChild(link);
+        indexList.appendChild(listItem);
 
-    link.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(`#${heading.id}`).scrollIntoView({ behavior: "smooth" });
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            document.querySelector(`#${heading.id}`).scrollIntoView({ behavior: "smooth" });
+        });
     });
-});
 
-indexListContainer.appendChild(indexList);
+    toggleButton.addEventListener("click", function() {
+        if (indexList.style.display === "none") {
+            indexList.style.display = "block";
+            toggleButton.innerHTML = '<i class="fa-solid fa-bars"></i>';
+        } else {
+            indexList.style.display = "none";
+            // indexListContainer.style.min-height="20px";
+            toggleButton.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+        }
+    });
+
+    indexListContainer.appendChild(indexList);
 });
