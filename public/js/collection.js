cab086c74c9d9ba2223eb19ec900a604a32a6ed3
PhanGB <113229252+PhanGB@users.noreply.github.com>
2024-11-16 00:05:55 +0700

    Delete public directory

--- public/js/collection.js
+++
@@ -1,9 +0,0 @@
-function filterCategory() {
-    const filterCategoryBox = document.getElementById('filterCategoryBox');
-    filterCategoryBox.classList.toggle('filter-menu__body--active');
-}
-
-function filterPrice() {
-    const filterPriceBox = document.getElementById('filterPriceBox');
-    filterPriceBox.classList.toggle('filter-menu__body--active');
-}
