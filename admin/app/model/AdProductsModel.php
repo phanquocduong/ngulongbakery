d5f0d745debd087350d844e7f619cffc8e73e37e
PhanGB <taolaai2468@gmail.com>
2024-11-17 23:27:14 +0700

    update file in folder admin 23h26

--- admin/app/model/AdProductsModel.php
+++ admin/app/model/AdProductsModel.php
@@ -11,7 +11,7 @@ class AdProductsModel
     {
         $sql = "SELECT products.*, categories.name AS category_name
                 FROM products
-                JOIN categories ON products.category_id = categories.id";
+                JOIN categories ON products.category_id = categories.id ORDER BY products.id DESC";
         return $this->db->getAll($sql);
     }
     public function getProductById($id)
@@ -35,6 +35,24 @@ class AdProductsModel
             JOIN categories ON products.category_id = categories.id
             ORDER BY products.stock_quantity DESC";
         return $this->db->getAll($sql);
+    }
+    public function insertPro($data)
+    {
+        $sql = "INSERT INTO products (name, price, sale, image, extra_image, short_description, detailed_description, rating, stock_quantity, sold, views, tags, status, category_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        $param = [$data['name'], $data['price'], $data['sale'], $data['image'], $data['extra_image'], $data['short_description'], $data['detailed_description'], $data['rating'], $data['stock_quantity'], $data['sold'], $data['views'], $data['tags'], $data['status'], $data['category_id']];
+        return $this->db->insert($sql, $param);
+    }
+    public function updatePro($id, $data)
+    {
+        $sql = "UPDATE products SET name = ?, price = ?, sale = ?, image = ?, extra_image = ?, short_description = ?, detailed_description = ?, rating = ?, stock_quantity = ?, sold = ?, views = ?, tags = ?, status = ?, category_id = ? WHERE id = ?";
+        $param = [$data['name'], $data['price'], $data['sale'], $data['image'], $data['extra_image'], $data['short_description'], $data['detailed_description'], $data['rating'], $data['stock_quantity'], $data['sold'], $data['views'], $data['tags'], $data['status'], $data['category_id'], $id];
+        return $this->db->update($sql, $param);
+    }
+    function deletePro($id)
+    {
+        $sql = "DELETE FROM products WHERE id = ?";
+        $param = [$id];
+        return $this->db->delete($sql, $param);
     }
 }
 
