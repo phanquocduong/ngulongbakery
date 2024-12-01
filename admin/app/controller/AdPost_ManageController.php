f78732513c15c38ec87a28c3389fade967e0c367
tricong2002 <truongnvps38421@gmail.com>
2024-12-01 22:58:38 +0700

    truong1

--- admin/app/controller/AdPost_ManageController.php
+++ admin/app/controller/AdPost_ManageController.php
@@ -19,8 +19,7 @@ class AdPost_ManageController
         $this->data['post'] = $this->post->getPost();
         $this->renderview('post_manage', $this->data);
     }
-    public function viewAddPost()
-    {
+    public function viewAddPost(){
         $this->renderview('add_post', $this->data);
     }
     public function viewPost_Detail()
@@ -47,8 +46,7 @@ class AdPost_ManageController
         }
     }
 
-    public function viewEditPost()
-    {
+    public function viewEditPost(){
         $this->renderview('edit_post', $this->data);
     }
     public function addPost()
@@ -66,54 +64,51 @@ class AdPost_ManageController
             $files = '../public/upload/post/images/' . $data['image'];
             if (!move_uploaded_file($_FILES['avt-post']['tmp_name'], $files)) {
                 echo '<script>alert("Upload ảnh không thành công")</script>';
-                echo '<script>location.href="index.php?page=add_post"</script>';
-                return;
+                var_dump($files);
+               /*  echo '<script>location.href="index.php?page=add_post"</script>'; */
+               /*  return; */
             }
             $this->post->insertPost($data);
             echo '<script>alert("Thêm bài viết thành công")</script>';
             echo '<script>location.href="index.php?page=post_manage"</script>';
         }
     }
-    public function editPost()
-    {
-        if (isset($_POST['submit'])) {
-            $data = [];
-            $data['id'] = $_POST['id'];
-            $data['title'] = $_POST['title'];
+        public function editPost(){
+            if(isset($_POST['submit'])){
+                $data = [];
+                $data['id'] = $_POST['id'];
+                $data['title'] = $_POST['title'];
 
             if (!empty($_FILES['avt-post1']['name'])) {
                 $data['image'] = $_FILES['avt-post1']['name'];
                 $file = '../public/upload/post/images/' . $data['image'];
                 if (!move_uploaded_file($_FILES['avt-post1']['tmp_name'], $file)) {
                     echo '<script>alert("Không thể tải lên ảnh bìa.")</script>';
-                    exit;
+                    return;
                 }
             } else {
-                $data['image'] = !empty($_POST['existing_image']) ? 
-                    htmlspecialchars(trim($_POST['existing_image'])) : 
-                    'default-post-image.jpg'; // Default image if none exists
+                $data['image'] = $_POST['existing_image'] ?? '';
             }
 
-            $data['content'] = $_POST['content'];
-            $data['created_at'] = $_POST['create_date'];
-            $data['status'] = $_POST['status'];
-            $data['author_id'] = $_POST['user'];
-            $data['category_id'] = $_POST['type'];
-            $this->post->updatePost($data);
-            echo '<script>alert("Sửa bài viết thành công")</script>';
+                $data['content'] = $_POST['content'];
+                $data['created_at'] = $_POST['create_date'];
+                $data['status'] = $_POST['status'];
+                $data['author_id'] = $_POST['user'];
+                $data['category_id'] = $_POST['type'];
+                $this->post->updatePost($data);
+                echo '<script>alert("Sửa bài viết thành công")</script>';
+                echo '<script>location.href="index.php?page=post_manage"</script>';
+            }
+        }
+
+        public function deletePost(){
+            if ($this->post->isForeignKey($_GET['id'])) {
+                $this->post->deleteRelatedData($id);
+            }
+            $id = isset($_GET['id']) ? $_GET['id'] : 0;
+            $this->post->deletePost($id);
+            echo '<script>alert("Xóa bài viết thành công")</script>';
             echo '<script>location.href="index.php?page=post_manage"</script>';
         }
     }
-
-    public function deletePost()
-    {
-        $id = isset($_GET['id']) ? $_GET['id'] : 0;
-        if ($this->post->isForeignKey($id)) {
-            $this->post->deleteRelatedData($id);
-        }
-        $this->post->deletePost($id);
-        echo '<script>alert("Xóa bài viết thành công")</script>';
-        echo '<script>location.href="index.php?page=post_manage"</script>';
-    }
-}
 ?>
